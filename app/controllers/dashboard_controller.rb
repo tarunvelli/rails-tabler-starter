1f5602472060c5d194841ff6646419b228d86c07
Tarun Vellishetty <tarunvelli@gmail.com>
2022-10-27 15:53:13 +0530

    rename welcome to dashboard

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,3 @@
+class DashboardController < ApplicationController
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "welcome#index"
+  root 'dashboard#index'
 end
