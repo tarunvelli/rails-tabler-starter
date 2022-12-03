4958b42e3d33b998b0839ae7ea3f9ed411de9365
Tarun Vellishetty <tarunvelli@gmail.com>
2022-12-03 18:57:44 +0530

    add apex charts via cdn

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import * as tabler from "./tabler"
 import * as bootstrap from "bootstrap"
