917845941e8dbaea08fddfb3687f8dd865c6a24e
Tarun Vellishetty <tarunvelli@gmail.com>
2022-12-12 08:02:29 +0530

    validate space before saving user_role

--- app/models/space.rb
+++ app/models/space.rb
@@ -5,19 +5,10 @@ class Space < ApplicationRecord
   has_many :users, through: :user_roles
 
   validates :name, presence: true
-  validate :check_multi_tenant_mode
 
   enum status: %i[active archived]
 
   def all_roles
     Role.where(space_id: [nil, id])
-  end
-
-  private
-
-  def check_multi_tenant_mode
-    return if multi_tenant_mode?
-
-    errors.add(:base, "Can't create additional spaces in single-tenant mode")
   end
 end
--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -2,4 +2,15 @@ class UserRole < ApplicationRecord
   belongs_to :user
   belongs_to :space
   belongs_to :role
+
+  validate :role_belongs_to_space
+
+  private
+
+  def role_belongs_to_space
+    return if role.common?
+    return if role.space == space
+
+    errors.add(:base, 'invalid role for space')
+  end
 end
