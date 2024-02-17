641e33405fe553ed45f316b46ad09b7e2f58f137
Tarun Vellishetty <tarunvelli@gmail.com>
2024-02-18 00:09:46 +0530

    Add env variable to override app settings default

--- config/initializers/load_app_settings.rb
+++ config/initializers/load_app_settings.rb
@@ -1,1 +1,7 @@
-Rails.application.config.app_settings = YAML.load_file(Rails.root.join("config/app_settings.yml"))
+Rails.application.config.app_settings = YAML.load_file(
+  Rails.root.join("config/app_settings.yml"),
+).tap do |setting|
+  setting.each do |key, value|
+    value["default"] = ENV[key.upcase] || value["default"]
+  end
+end
