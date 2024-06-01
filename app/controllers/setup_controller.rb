954f95088b7de1ef16c81918f003ff42997d0f8a
Tarun Vellishetty <tarunvelli@gmail.com>
2024-06-01 12:27:42 +0530

    Fix demo setup controller

--- app/controllers/setup_controller.rb
+++ app/controllers/setup_controller.rb
@@ -6,14 +6,26 @@ class SetupController < ApplicationController
   end
 
   def update
-    app_settings = params["app_settings"]
-
     respond_to do |format|
-      if AppSettings.update(app_settings.keys, app_settings.values)
+      if AppSettings.update(keys, values)
         format.html { redirect_to edit_setup_path, notice: "Settings were successfully updated." }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
     end
+  end
+
+  private
+
+  def setup_params
+    params.require(:app_settings).permit!
+  end
+
+  def keys
+    setup_params.keys
+  end
+
+  def values
+    setup_params.values.map(&:to_h)
   end
 end
