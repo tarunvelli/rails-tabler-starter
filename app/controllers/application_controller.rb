78095287e3e367894e099d34201172faddfd2f85
Ganesh Kunwar <gkunwar09@gmail.com>
2025-08-07 11:08:14 +0545

    Bug fix: fixed error from pundit gem #19

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
   include SettingsHelper
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
