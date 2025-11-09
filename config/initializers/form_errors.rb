4a1bd341d4e3886ca8f1f988618ff2797b592c42
Aleksandar <aleksandar@zoric.me>
2025-11-09 10:14:16 +0100

    Improved error messages

---
+++ config/initializers/form_errors.rb
@@ -0,0 +1,17 @@
+# Inline form-field error messages
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  if html_tag =~ /type="hidden"/ || html_tag =~ /<label/
+    html_tag
+  else
+    html_tag.gsub!("form-control", "form-control is-invalid")
+    
+    %{<div class="field-with-errors">
+        #{html_tag}
+        <span class="invalid-feedback">
+          #{[instance.error_message].flatten.join(', ')}
+        </span>
+      </div>
+    }.html_safe
+  end
+end
