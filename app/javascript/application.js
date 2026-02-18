3ea3e1ccc8e80280ea6eeb620f7fdda299d90c3c
Tarun Vellishetty <tarunvelli@gmail.com>
2026-02-19 01:44:58 +0530

    Fix bootstrap dropdowns

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import { tabler } from "@tabler/core"
+import { tabler, bootstrap } from "@tabler/core"
 import * as jsVectorMap from "jsvectormap"
 import * as world from "world"
-import * as bootstrap from "bootstrap"
 import ApexCharts from "apexcharts"
 
 window.tabler = tabler
+window.bootstrap = bootstrap
 window.ApexCharts = ApexCharts
 
 Turbo.session.drive = false
